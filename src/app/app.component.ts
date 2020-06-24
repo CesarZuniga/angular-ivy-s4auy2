f9b2f45468e3a59dcc2aa37da5baa58bc2bc333c
CesarZuniga <cesaruriel007@gmail.com>
2020-06-24 16:39:17 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent  {
   name = 'Muestre en pantalla los números del 1 al 100, sustituyendo los múltiplos de 3 por la palabra “Fizz” y, a su vez, los múltiplos de 5 por “Buzz”. Para los números que son múltiplos de 3 y 5 (como 15, 30, etc), utiliza el la palabra “FizzBuzz';
   numeros = new Array<Modelo>();
+  ejercicio: number;
    constructor(){
      this.checarMultiplosDe3y5(100);
    }
@@ -24,7 +25,12 @@ export class AppComponent  {
       this.numeros.push(element);      
     }
   }
+  setEjercicio(e: any){
+    this.ejercicio = e;
+    console.log(e);
+  }
 }
+
 export class Modelo{
   numero: number;
   descripcion = '';
