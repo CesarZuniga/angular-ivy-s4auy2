416a0301d7631501037e7c5f8095b2680e1d5dc0
CesarZuniga <cesaruriel007@gmail.com>
2020-06-24 17:22:35 -0500

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,10 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Muestre en pantalla los números del 1 al 100, sustituyendo los múltiplos de 3 por la palabra “Fizz” y, a su vez, los múltiplos de 5 por “Buzz”. Para los números que son múltiplos de 3 y 5 (como 15, 30, etc), utiliza el la palabra “FizzBuzz';
+  name = '';
   numeros = new Array<Modelo>();
   ejercicio: number;
+  esPalidromoDes = '';
    constructor(){
      this.checarMultiplosDe3y5(100);
    }
@@ -25,9 +26,36 @@ export class AppComponent  {
       this.numeros.push(element);      
     }
   }
-  setEjercicio(e: any){
+  setEjercicio(e: number){
     this.ejercicio = e;
-    console.log(e);
+     switch (e) {
+     case 1:
+       this.name = 'Probar si una palabra es palíndromo o no'; 
+       break;
+     case 2:
+       this.name = 'Muestre en pantalla los números del 1 al 100, sustituyendo los múltiplos de 3 por la palabra “Fizz” y, a su vez, los múltiplos de 5 por “Buzz”. Para los números que son múltiplos de 3 y 5 (como 15, 30, etc), utiliza el la palabra “FizzBuzz”'; 
+       break;  
+     default:
+       break;
+   }
+  }
+  esPalindromo(valor: string){
+    if(valor){
+      valor.trim();
+      this.esPalidromoDes = this.verificaSiEsPalindromo(valor);
+
+    }else{
+      this.esPalidromoDes = '';
+    }
+  }
+  verificaSiEsPalindromo(valor: string) :string {
+    let aux = '';
+    const longitud = valor.length - 1;
+    const arrChar = valor.split('');
+    for (let index = longitud; index >= 0; index--) {
+      aux = aux + arrChar[index];
+    }
+    return valor === aux ? 'SI' : 'NO';
   }
 }
 
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h3>{{name}}!</h3>`,
+  template: `<h3>{{name}}</h3>`,
   styles: [`h3 { font-family: Lato; }`]
 })
 export class HelloComponent  {
